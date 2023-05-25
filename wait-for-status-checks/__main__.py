954bc49e20026198b675a5cd21f123bbcc511f1e
omkarkhatavkar <okhatavkar007@gmail.com>
2023-05-25 17:02:58 +0530

    updated the api_endpoint to global

--- wait-for-status-checks/__main__.py
+++ wait-for-status-checks/__main__.py
@@ -6,8 +6,8 @@ import time
 import sys
 from pathlib import Path
 
-outputs_path = Path(os.environ['GITHUB_OUTPUT'])
-summary_path = Path(os.environ['GITHUB_STEP_SUMMARY'])
+outputs_path = Path(os.environ["GITHUB_OUTPUT"])
+summary_path = Path(os.environ["GITHUB_STEP_SUMMARY"])
 
 
 def str_to_int(value):
@@ -18,7 +18,7 @@ def str_to_int(value):
 
 
 def write_to_summary(message, is_error=False):
-    with summary_path.open(mode='a') as summary_file:
+    with summary_path.open(mode="a") as summary_file:
         if is_error:
             sys.stderr.write(message + "\n")
         else:
@@ -33,8 +33,8 @@ def set_gha_output(name, value):
     * https://hynek.me/til/set-output-deprecation-github-actions/
     * https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
     """
-    with open(outputs_path, 'a') as outputs_file:
-        print(f'{name}={value}', file=outputs_file, end="\n")
+    with open(outputs_path, "a") as outputs_file:
+        print(f"{name}={value}", file=outputs_file, end="\n")
 
 
 def get_status():
@@ -49,49 +49,67 @@ def get_context():
     return context
 
 
-def main(args):
+def main(arguments):
     response = requests.get(api_endpoint)
     statuses_length = len(json.loads(response.text))
     if statuses_length == 0:
-        write_to_summary(f"{args.context} failed to start! Stopping.", is_error=True)
-        set_gha_output('result', 'failure')
+        write_to_summary(
+            f"{arguments.context} failed to start! Stopping.", is_error=True
+        )
+        set_gha_output("result", "failure")
         sys.exit(1)
     status = get_status()
     context = get_context()
-    if context != args.context:
-        write_to_summary(f"{args.context} failed to start! Stopping.", is_error=True)
-        set_gha_output('result', 'failure')
+    if context != arguments.context:
+        write_to_summary(
+            f"{arguments.context} failed to start! Stopping.", is_error=True
+        )
+        set_gha_output("result", "failure")
         sys.exit(1)
     counter = 0
-    write_to_summary(f"Waiting for {args.context} to complete...")
+    write_to_summary(f"Waiting for {arguments.context} to complete...")
     while status != "success" and status != "failure":
-        if counter > args.count:
-            write_to_summary(f"{args.context} Timeout! Stopping.", is_error=True)
-            set_gha_output('result', 'failure')
+        if counter > arguments.count:
+            write_to_summary(f"{arguments.context} Timeout! Stopping.", is_error=True)
+            set_gha_output("result", "failure")
             sys.exit(1)
-        time.sleep(args.wait)
+        time.sleep(arguments.wait)
         status = get_status()
-        write_to_summary(f"{args.context}: {status}")
+        write_to_summary(f"{arguments.context}: {status}")
         counter += 1
 
     if status != "success":
-        write_to_summary(f"{args.context}: {status}")
+        write_to_summary(f"{arguments.context}: {status}")
         write_to_summary("::error PRT failed", is_error=True)
-        set_gha_output('result', 'failure')
+        set_gha_output("result", "failure")
         sys.exit(1)
     else:
-        write_to_summary(f"{args.context} Passed Successfully!")
-        set_gha_output('result', 'success')
+        write_to_summary(f"{arguments.context} Passed Successfully!")
+        set_gha_output("result", "success")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="GitHub Actions that waits for the GitHub Status API result")
-    parser_github = parser.add_argument_group("Status API")
-    parser_github.add_argument("--context", required=True, type=str, help="Context of Status API")
-    parser_github.add_argument("--wait-interval", type=str_to_int, default=300, dest="wait",
-                               help="Time to wait between polling (in seconds)")
-    parser_github.add_argument("--count", type=str_to_int, default=12, help="Number of times to poll before timing out")
-    parser_github.add_argument("--ref", type=str, help="Github commit head ref.")
+    parser = argparse.ArgumentParser(
+        description="GitHub Actions that waits for the GitHub Status API result"
+    )
+    parser.add_argument(
+        "--context", required=True, type=str, help="Context of Status API"
+    )
+    parser.add_argument(
+        "--wait-interval",
+        type=str_to_int,
+        default=300,
+        dest="wait",
+        help="Time to wait between polling (in seconds)",
+    )
+    parser.add_argument(
+        "--count",
+        type=str_to_int,
+        default=12,
+        help="Number of times to poll before timing out",
+    )
+    parser.add_argument("--ref", type=str, help="Github commit head ref.")
     arguments = parser.parse_args()
+    global api_endpoint
     api_endpoint = f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/statuses/{arguments.ref}"
     main(arguments)
